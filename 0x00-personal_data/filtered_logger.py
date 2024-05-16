ba962ad85caa5db041195b6fd16b9cae188adb01
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-16 14:07:29 +0300

    Log formatter and  Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,68 @@
 """ Regex-ing"""
 
 from typing import List
+import mysql.connector
+import logging
 import re
+import os
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """ function that returns the log messag obfuscated"""
     for field in fields:
-        pattern = re.sub(rf"{field}=(.*?)\{separator}",
-                         f'{field}={redaction}{separator}', message)
+        matchs = re.search(rf'{field}=(.*?){separator}', message)
+        if matchs:
+            pattern = re.sub(matchs.group(1), redaction, message, 1)
     return pattern
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initialization"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum"""
+        formatted_record = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            formatted_record, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ function that returns a logger object"""
+    user_data = logging.getLogger("user_data")
+    user_data.setLevel(logging.INFO)
+    user_data.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    user_data.addHandler(handler)
+    return user_data
+
+
+def get_db():
+    """ function that returns a connector to the database"""
+    host = os.getenv("PERSONAL_DATA_DB_HOST")
+    if host is None:
+        host = "localhost"
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    if user is None:
+        user = "root"
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    if password is None:
+        password = ""
+    db = os.getenv("PERSONAL_DATA_DB_NAME")
+    dbConnection = mysql.connector.connect(
+        host=host, user=user, password=password, database=db
+    )
+    return dbConnection
