0c64bd08b6effe495c5bb60648319c5ae6d9a421
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-22 00:12:37 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ Basic Auth class"""
+    """ Basic Authentication class"""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """ Extract base64 authorization header"""
