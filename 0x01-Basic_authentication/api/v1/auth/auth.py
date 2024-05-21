1e1e104cc4f56d0a368ce564be8f01d3fb3effa1
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-21 11:45:03 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+""" Auth class """
+
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """Auth class
-    """
+    """Authentication class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth
