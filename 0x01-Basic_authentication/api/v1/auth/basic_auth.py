01dd8a79e0613d24d658f1b40e509ce8413adb2e
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-22 00:20:17 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+""" Basic Authentication class"""
+
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ Basic Authentication class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """ Extract base64 authorization header"""
         if authorization_header is None:
             return None
@@ -14,7 +19,8 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+                self, base64_authorization_header: str) -> str:
         """ Decode base64 authorization header"""
         if base64_authorization_header is None:
             return None
