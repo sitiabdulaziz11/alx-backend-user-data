b996bc93ed4a64617a94d81d444f261f5dc6d824
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-21 10:22:39 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,7 +13,11 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
-
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized"""
+    abort(401)
+    
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
