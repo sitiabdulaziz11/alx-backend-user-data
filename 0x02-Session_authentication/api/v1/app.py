6d12d87a108b90ad4812b90f162b35af0da961a1
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-24 02:14:51 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,10 +37,11 @@ def before_request() -> str:
         return
     notsubset_path = ['/api/v1/status/',
                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
-
+    notsubset_path.append('/api/v1/auth_session/login')
     if auth.require_auth(request.path, notsubset_path) is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) and auth. session_cookie(
+            request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
