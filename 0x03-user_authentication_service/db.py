3570b3bb7cbd4367a1cdd73c01efe89a5dc8785d
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-28 11:13:35 +0300

    create or add and Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """DB module
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -16,8 +18,10 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
+        """ useful during development or
+        for testing when you might need to reset the database schema."""
         Base.metadata.create_all(self._engine)
         self.__session = None
 
@@ -32,7 +36,20 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> None:
         """ Adds a new user to the database"""
+        if not email or not hashed_password:
+            return
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Returns the first row found in the users table"""
+        if not kwargs:
+            raise InvalidRequestError
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).one()
+        except NoResultFound as e:
+            raise e
+        return user
