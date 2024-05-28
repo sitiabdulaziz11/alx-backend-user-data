e360e7d8c23ff295790789b46170062094ac2651
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-28 12:30:45 +0300

    Register user and Basic Flask app

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Hash password with bcrypt"""
+
+import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    """Return a salted, hashed password"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Initialize a new Auth instance"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a new user"""
+        if not email or not password:
+            return
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+       
